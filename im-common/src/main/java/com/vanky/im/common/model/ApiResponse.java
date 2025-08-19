f9c2ab620c18cb5e2918a1d8822de4cab00a8e61
vankykoo <3541424964@qq.com>
2025-08-19 15:54:33 +0800

    Update ApiResponse.java

--- im-common/src/main/java/com/vanky/im/common/model/ApiResponse.java
+++ im-common/src/main/java/com/vanky/im/common/model/ApiResponse.java
@@ -1,6 +1,8 @@
 package com.vanky.im.common.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author vanky
@@ -8,16 +10,12 @@ import lombok.Data;
  * @description 通用API响应对象
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApiResponse<T> {
     private int code;
     private String message;
     private T data;
-
-    private ApiResponse(int code, String message, T data) {
-        this.code = code;
-        this.message = message;
-        this.data = data;
-    }
 
     public static <T> ApiResponse<T> success(T data) {
         return new ApiResponse<>(200, "success", data);
